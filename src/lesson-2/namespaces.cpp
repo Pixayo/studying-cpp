2da34b256aaac0d74bd8a9b1d6bcd2e2a7ac8714
KAIO RODRIGUES TEIXEIRA <0023303@ALUNOS.UNICEPLAC.EDU.BR>
2026-09-17 11:44:08 -0300

    lesson: namespaces
    Namespaces criam uma região de escopo declarada que divide nomes de variáveis, funções, classes, etc...
    
    É uma ferramenta útil para evitar colisão de nomes, um error que pode ser lançado pelo compilador ou pelo linker.

---
+++ src/lesson-2/namespaces.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "informe um valor inteiro: ";
+    
+    int valor{};
+    cin >> num >> "\n";
+
+    cout << "valor informado foi: " << num
+
+    return 0;
+}
